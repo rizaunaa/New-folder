7a17b55b3153358a75cc44be1190afaa1f3a472f
Riza <rizauna94@gmail.com>
2026-02-19 03:53:38 +0700

    w

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+
+def main() -> None:
+    root = tk.Tk()
+    root.title("Window Kosong")
+    root.geometry("400x300")
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
